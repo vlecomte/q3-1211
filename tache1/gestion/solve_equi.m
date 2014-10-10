75d91eccb9c67b6b061046744aa4b17498bb92a7
Victor Lecomte <vjavadore@gmail.com>
2014-10-10 20:39:04 +0200

    Went for an approach that depends less on the implementation of null()

--- tache1/gestion/solve_equi.m
+++ tache1/gestion/solve_equi.m
@@ -1,10 +1,13 @@
 function abc = solve_equi(Kp1, Kp2, out4)
 
-syms a b real;
-param = param_fluxes();
-c = out4 / param(7,3);
+syms a b c real;
 
+param_gen = param_fluxes();
 param_eq = param_fluxes_eq();
+
+% Equates flux at out4 to find c
+n_gen = param_gen * [a;b;c];
+c = solve([0 0 0 0 0 0 1 0 0 0 0 0]*n_gen == out4);
 
 % Solves equilibrium of alpha for a
 n_eq = param_eq * [a;b;c];
@@ -16,6 +19,6 @@ b = solve(n_eq(2)^3*n_eq(3) / (n_eq(1)*n_eq(5)*sum(n_eq)^2) == Kp1, n_eq(3) >= 0
 
 a = eval(a);
 
-abc = [double(a);double(b);c];
+abc = [double(a);double(b);double(c)];
 
 end
