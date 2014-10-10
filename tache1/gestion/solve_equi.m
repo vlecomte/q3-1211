f02c91a59452489ba27ed8fed35ea57ab170fa90
Victor Lecomte <vjavadore@gmail.com>
2014-10-11 01:15:23 +0200

    Removed an incredibly useless scalar product

--- tache1/gestion/solve_equi.m
+++ tache1/gestion/solve_equi.m
@@ -7,7 +7,7 @@ param_eq = param_fluxes_eq();
 
 % Equates flux at out4 to find c
 n_gen = param_gen * [a;b;c];
-c = solve([0 0 0 0 0 0 1 0 0 0 0 0]*n_gen == out4);
+c = solve(n_gen(7) == out4, c);
 
 % Solves equilibrium of alpha for a
 n_eq = param_eq * [a;b;c];
@@ -16,7 +16,7 @@ a = solve(n_eq(2)*n_eq(4) / (n_eq(3)*n_eq(5)) == Kp2, a);
 % Solves equilibrium of beta for b
 n_eq = param_eq * [a;b;c];
 b = solve(n_eq(2)^3*n_eq(3) / (n_eq(1)*n_eq(5)*sum(n_eq)^2) == Kp1, ...
-    n_eq(3) >= 0);
+    n_eq(3) >= 0, b);
 
 % Converting to doubles
 b = double(b);
